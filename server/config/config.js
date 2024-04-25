50a3a2e376f2ec0fb8b99cd8689163b5a9330f3d
Blanche <peltierblanche@gmail.com>
2024-04-25 09:59:30 +0200

    Delete server directory

--- server/config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    jwtSecret: '0243822a9ef8edabf23376de4d1ba51cd877fc3bd971f3f28ccb9a457e30a896', // Clé secrète pour JWT (à remplacer par une valeur sécurisée)
-    mongoURI: 'mongodb://localhost:27017/social_network'};
